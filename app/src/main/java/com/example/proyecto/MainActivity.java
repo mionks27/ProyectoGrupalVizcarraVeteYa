ccd4330afaf64f5316f28688b406d544ebf2ec02
Julio Carrion <julio@itic.com.pe>
2020-11-23 00:17:18 -0500

    FireBase Authentication completo, solo falta corregir un error que preguntaré al profe para verificar el email

--- app/src/main/java/com/example/proyecto/MainActivity.java
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -1,8 +1,26 @@
 package com.example.proyecto;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthMethodPickerLayout;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +28,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        validarUsuario();
     }
+
+    public void login(View view){
+
+        List<AuthUI.IdpConfig> prooviders = Arrays.asList(
+                new  AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+
+        AuthUI instance = AuthUI.getInstance();
+        Intent intent = instance.createSignInIntentBuilder().setAvailableProviders(prooviders).build();
+
+        startActivityForResult(intent,1);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode ==1 ){
+            validarUsuario();
+        }
+
+    }
+
+
+
+    public void  validarUsuario(){
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null){
+            firebaseUser.reload();
+            String uid = firebaseUser.getUid();
+            String name = firebaseUser.getDisplayName();
+            String email = firebaseUser.getEmail();
+
+            Log.d("info", uid + " " + name + " " + email);
+
+        }
+//        else {
+//            Toast.makeText(this, "Se le ha enviado un correo para verificar su cuenta", Toast.LENGTH_SHORT);
+//            firebaseUser.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+//                @Override
+//                public void onComplete(@NonNull Task<Void> task) {
+//                    Log.d("emailVer", "Correo enviado");
+//                }
+//            });
+//        }
+    }
+
+    public void logOut(View view){
+        AuthUI instance = AuthUI.getInstance();
+        instance.signOut(this).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+             // Lógica de cerrao de sesión lo pongo aquí porque luego lo ecesitaremos cuando acabemos el menú de cliente y TI
+            }
+        });
+    }
+
+
 }
