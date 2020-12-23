a6e0217c8279c913b195edcc2d87086b720f6ae5
Royer <r.yangali@pucp.pe>
2020-12-22 19:55:41 -0500

    agregando pop up menu TI

--- app/src/main/java/com/example/proyecto/ti/PaginaPrincipalTI.java
+++ app/src/main/java/com/example/proyecto/ti/PaginaPrincipalTI.java
@@ -1,8 +1,13 @@
 package com.example.proyecto.ti;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
 
 import com.example.proyecto.R;
 
@@ -12,5 +17,48 @@ public class PaginaPrincipalTI extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagina_principal_t_i);
+    }
+
+
+    ////para relacionar el layout de menú con esta vista
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_ti, menu);
+        return true;
+    }
+
+    ///para linkear las opciones del menú con una acción en particular de forma centralizada ///también puede realizarse desde el primer método onCreate pero de otra manera, revisar min 01:18:43 del video zoom
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.abrirMenu:
+                View view = findViewById(R.id.abrirMenu);
+                PopupMenu popupMenu = new PopupMenu(this, view);
+                popupMenu.getMenuInflater().inflate(R.menu.menu_popup_ti, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        switch (menuItem.getItemId()){
+                            case R.id.verSolicitudPrestamoTI:
+                                return true;
+                            case R.id.verPedidosTI:
+                                return true;
+                            case R.id.gestionarDispositivosTI:
+                                return true;
+                            case R.id.cerrarSesionTI:
+                                return true;
+                            default:
+                                return false;
+
+                        }
+                    }
+                });
+                popupMenu.show();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
