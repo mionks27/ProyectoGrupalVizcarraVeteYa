5c3afc6b7dc273031db25c08b1793e2e21f6a524
Royer <r.yangali@pucp.pe>
2021-01-01 17:23:31 -0500

    arreglando error cuando gps está apagado

--- app/src/main/java/com/example/proyecto/Cliente/SolicitudReserva.java
+++ app/src/main/java/com/example/proyecto/Cliente/SolicitudReserva.java
@@ -8,6 +8,7 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
@@ -27,6 +28,7 @@ import android.widget.Toast;
 
 import com.example.proyecto.Entity.Device;
 import com.example.proyecto.R;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -60,42 +62,44 @@ public class SolicitudReserva extends AppCompatActivity {
         TextView textViewtitulo = findViewById(R.id.textViewTituloDisp);
         textViewtitulo.setText(device.getMarca() + ' ' + device.getTipo());
 
-        ///PARA GPS
-        mostrarInfoDeUbicacion();
-
     }
 
-    private void mostrarInfoDeUbicacion() {
+    public void mostrarInfoDeUbicacion(View view) {
+        if (gpsActivo()) {
+            int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+            if (permission == PackageManager.PERMISSION_GRANTED) {
+                FusedLocationProviderClient location = LocationServices.getFusedLocationProviderClient(this);
+                location.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        Log.d("infoApp", "ALt" + location.getAltitude());
+                        Log.d("infoApp", "Lat" + location.getLatitude());
+                        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                        try {
+                            List<Address> direccion = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                            Log.d("infoApp", "la direccion es:" + direccion.get(0).getAddressLine(0));
+                            TextView textViewGps = findViewById(R.id.textviewdireccionGPS);
+                            textViewGps.setText(direccion.get(0).getAddressLine(0));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
 
-        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
-        if (permission == PackageManager.PERMISSION_GRANTED) {
-            FusedLocationProviderClient location = LocationServices.getFusedLocationProviderClient(this);
-            location.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    Log.d("infoApp", "ALt" + location.getAltitude());
-                    Log.d("infoApp","Lat" + location.getLatitude());
-                    Geocoder geocoder = new Geocoder(getApplicationContext(),Locale.getDefault());
-                    try {
-                        List <Address> direccion = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
-                        Log.d("infoApp","la direccion es:" + direccion.get(0).getAddressLine(0));
-                        TextView textViewGps = findViewById(R.id.textviewdireccionGPS);
-                        textViewGps.setText(direccion.get(0).getAddressLine(0));
-                    } catch (IOException e) {
-                        e.printStackTrace();
                     }
+                });
+                location.getLastLocation().addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("infoApp", "Fallo aquí GA");
+                    }
+                });
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }else {
+            Toast.makeText(SolicitudReserva.this, "Por favor active su GPS", Toast.LENGTH_SHORT).show(); //FORMATO DE UN TOAST QUE ES COMO UN POP UP
 
-                }
-            });
-            location.getLastLocation().addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Log.d("infoApp", "Fallo aquí GA");
-                }
-            });
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
+
 
     }
 
@@ -104,12 +108,17 @@ public class SolicitudReserva extends AppCompatActivity {
         if (requestCode == 1) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Log.d("infoApp", "Permisos concedidos");
-                mostrarInfoDeUbicacion();
             } else {
                 Log.d("infoApp", "Persmisos denegados");
             }
 
         }
+    }
+
+    private boolean gpsActivo() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        boolean providerEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        return providerEnabled;
     }
 
     public void confirmarReserva(View view) {
